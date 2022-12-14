7aae943407fa2840e40d453a9f9f4b828199f6ef
ZeRX1 <kacper@zacharewicz.info>
2022-12-14 08:55:20 +0100

    uga booga

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-import os
+import os, json, time
 
 def readFiles(patha):
     res = []
@@ -13,6 +13,32 @@ def readFiles(patha):
         else:
             pathf = patha + path + '/'
             res.append(readFiles(pathf))
-
-
     return res
+
+def saveJson(array, name):
+    with open(f'{name}.json', 'w') as outfile:
+        outfile.write(json.dumps(tuple(array),indent=4))
+    return "Data saved"
+
+def createConfig():
+    print("Welcome to config creator!")
+    folders_amount = int(input("How many folders would you like to account in backup: \n"))
+    folders = dict()
+    while folders_amount > 0:
+        print(str(folders_amount) + " left to assign")
+        folders.append(({
+        'path': input("What's the name of the path? \n")
+        }))
+        os.system('cls')
+        folders_amount -= 1
+    with open('config.json', 'w') as outfile:
+        outfile.write(json.dumps(tuple(folders),indent=4))
+    print("Config succesfully created!")
+    print("Restart the script with no arguments to use config as data used!")
+    time.sleep(2)
+    return
+
+
+def backupFiles():
+    print('asdf')
+    return
--- main.py
+++ main.py
@@ -1,8 +1,74 @@
-import os
+import os, time, sys, getopt
 from functions import *
+from os.path import exists
 
+
+def main(d):
+
+    if not d:
+        if exists("config.json"):
+            with open("config.json") as input:
+                if not input:
+                    print("The config file is empty. Use -c to create one!")
+                    return
+                configjson = json.load(input)
+        else:
+            print("The config file doesn't exist. Use -c to create one!")
+            return
+        #here with config usage
+        
+    #here with arguments
+    if d.get("config") == True:
+        createConfig()
+        print('run the script again to use parameters from config')
+        return
+    elif d.get("backup") == True:
+        backupFiles()
+        print("backup ended successfully")
+        return
+
+    path = d['directory']
+    print(saveJson(readFiles(path), "previous_files"))
+        
+    return
+
+def readArgs(argv):
+    try:
+        opts, args = getopt.getopt(argv, "d:cb")
+    except getopt.GetoptError as err:
+        help_command()
+        print(err) 
+        sys.exit(2)
+
+    #add this to an array/dictionary and send further
+    d = dict()
+    for opt, arg in opts:
+        print(opt, arg)
+        if opt in ('-d'):
+            d['directory'] = arg
+        elif opt in ('-b'):
+            d['backup'] = True
+        elif opt in ('-c'):
+            d['config'] = True
+        print(d)
+    
+    main(d)
+    return
+
+def help_command():
+    print("Help")
+
+    return    
 
 if __name__ == "__main__":
-    path = os.getenv('path')
-    print(readFiles(path))
-    
+    try:
+        # read arguments
+        readArgs(sys.argv[1:])
+        time.sleep(2)
+    except (ValueError, KeyError, AttributeError, IndexError) as err:
+        print(err)
+    except TimeoutError as err:
+        print("Is the IP correct? (.env)" + err)
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+        raise
